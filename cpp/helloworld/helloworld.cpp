cba13a1f7fad9b66bd80117740e2d3cbb4747db3
PXY11 <1056827244@qq.com>
2022-02-27 13:47:34 +0800

    Recompile on mac

--- cpp/helloworld/helloworld.cpp
+++ cpp/helloworld/helloworld.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout<<"hello,world"<<endl;
+    cout<<"Hello,world!"<<endl;
+    cout<<"Test cpp run on mac"<<endl;
     cout<<"Run is end."<<endl;
     return 0;
 
